e123df4b8b70691970dd6260c326bf2a261fee70
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2024-10-23 11:52:28 +0000

    The Haddock documentation for the function that checks for enemies within range has been added, along with usage examples to illustrate its practical application.
    Co-authored-by: veralmeida23 <2310veraalmeida@gmail.com>

--- lib/Tarefa2.hs
+++ lib/Tarefa2.hs
@@ -11,7 +11,26 @@ module Tarefa2 where
 
 import LI12425
 
-------------------------------------------------------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------------------------------
+
+{-| a função `inimigosNoAlcance` calcula os inimigos ao alcance de uma dada torre. 
+
+=== Exemplo de Uso:
+
+* `torre` = Torre (5, 5) 25 3 3 2 0 (Projetil Fogo (Finita 7))
+
+* `listaInimigos` = [
+            Inimigo (3.0, 4.0) Norte 100.0 1.0 10.0 20 [] (3.0, 4.0) 0,
+            Inimigo (7.0, 5.0) Sul 80.0 1.0 15.0 30 [] (7.0, 5.0) 0,
+            Inimigo (10.0, 10.0) Este 50.0 1.5 20.0 40 [] (10.0, 10.0) 0
+          ]
+
+>>> inimigosNoAlcance torre listaInimigos     
+    [Inimigo {posicaoInimigo = (3.0,4.0), direcaoInimigo = Norte, vidaInimigo = 100.0, velocidadeInimigo = 1.0, ataqueInimigo = 10.0, butimInimigo = 20, projeteisInimigo = [], posInicial = (3.0,4.0), tempoInimigo = 0.0},
+    Inimigo {posicaoInimigo = (7.0,5.0), direcaoInimigo = Sul, vidaInimigo = 80.0, velocidadeInimigo = 1.0, ataqueInimigo = 15.0, butimInimigo = 30, projeteisInimigo = [], posInicial = (7.0,5.0), tempoInimigo = 0.0}] 
+
+-}
+
 
 inimigosNoAlcance :: Torre -> [Inimigo] -> [Inimigo]
 inimigosNoAlcance _ [] = []
@@ -20,4 +39,4 @@ inimigosNoAlcance t (i:li) | dist (posicaoTorre t) (posicaoInimigo i) <= alcance
 
     where dist (x1,y1) (x2,y2) = sqrt $ (x1-x2)^2 + (y1-y2)^2
 
-------------------------------------------------------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------------------------------
