d354f10f6def60961214640f4cd009df7f403c08
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2024-10-23 15:44:49 +0000

    Writing examples for tests. Enemy 2.
    Co-authored-by: veralmeida23 <2310veraalmeida@gmail.com>

--- spec/Tarefa2Spec.hs
+++ spec/Tarefa2Spec.hs
@@ -9,3 +9,7 @@ import Test.HUnit
 -- | Inimigo 1 para testes.
 inimigo01 :: Inimigo
 inimigo01 = Inimigo (3.0, 4.0) Norte 100 1 10 20 [Projetil Fogo (Finita 5)] (0.5,0.5) 0
+
+-- | Inimigo 2 para testes.
+inimigo02 :: Inimigo
+inimigo02 = Inimigo (7.0, 5.0) Sul 80 1 15 30 [Projetil Gelo (Finita 3)] (0.5,0.5) 0
