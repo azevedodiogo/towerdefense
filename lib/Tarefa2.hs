b2e4ed8f428df1a45f30ef3ad4f0bf76f825c31f
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2024-10-23 12:12:23 +0000

    The Haddock documentation for the function that updates an enemy's state whenever it is hit by a projectile has been added, along with usage examples to illustrate its practical application.
    Co-authored-by: veralmeida23 <2310veraalmeida@gmail.com>

--- lib/Tarefa2.hs
+++ lib/Tarefa2.hs
@@ -39,7 +39,20 @@ inimigosNoAlcance t (i:li) | dist (posicaoTorre t) (posicaoInimigo i) <= alcance
 
     where dist (x1,y1) (x2,y2) = sqrt $ (x1-x2)^2 + (y1-y2)^2
 
+
 ---------------------------------------------------------------------------------------------------------------------------------------
+
+
+{- | a função `atingeInimigo` atualiza o estado de um inimigo sempre que este é atingido por um projétil. 
+
+=== Exemplos de Uso:
+
+* `inimigo` = Inimigo (3.0, 4.0) Norte 100 1 10 20 [Projetil Fogo (Finita 5)] (0,0) 0
+* `torre` = Torre (5, 5) 25 3 3 2 0 (Projetil Fogo (Finita 7))
+
+>>> atingeInimigo torre inimigo 
+Inimigo {posicaoInimigo = (3.0,4.0), direcaoInimigo = Norte, vidaInimigo = 75.0, velocidadeInimigo = 1.0, ataqueInimigo = 10.0, butimInimigo = 20, projeteisInimigo = [Projetil {tipoProjetil = Fogo, duracaoProjetil = Finita 12.0}], posInicial = (0.0,0.0), tempoInimigo = 0.0}
+-}
 
 atingeInimigo :: Torre -> Inimigo -> Inimigo
 atingeInimigo t i = i
