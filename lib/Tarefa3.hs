850db369e74d1c58b8ba642229b33e13ede30d30
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2024-10-25 21:39:40 +0000

    Start of task 3.
    Co-authored-by: veralmeida23 <2310veraalmeida@gmail.com>

---
+++ lib/Tarefa3.hs
@@ -0,0 +1,16 @@
+{-|
+Module      : Tarefa3
+Description : Mecânica do Jogo
+Copyright   : Diogo Matos Azevedo <a109727@alunos.uminho.pt>
+              Vera da Silva Almeida <a110723@alunos.uminho.pt>
+
+-- O propósito desta tarefa é simular um modelo de jogo, com um foco nos movimentos dos inimigos e na interação destes com as torres e a base.
+Módulo para a realização da Tarefa 3 de LI1 em 2024/25.
+-}
+
+module Tarefa3 where
+
+import Tarefa1
+import Tarefa2
+import LI12425
+import Data.List
