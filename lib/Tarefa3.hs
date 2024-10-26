49dd874f31eee7f7e3af427abb456a200b7c5db8
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2024-10-26 14:23:31 +0000

    Haddock documentation for the function that simulates the evolution of a game after a time interval has been added, along with usage examples to illustrate its practical application.
    Co-authored-by: veralmeida23 <2310veraalmeida@gmail.com>

--- lib/Tarefa3.hs
+++ lib/Tarefa3.hs
@@ -15,6 +15,39 @@ import Tarefa2
 import LI12425
 import Data.List
 
+--------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+{- | a função `atualizaJogo` simula a evolução de um jogo após um intervalo de tempo. 
+
+As atualizações são realizadas em três etapas principais:
+   
+1. Atualização dos portais, adicionando novos inimigos ao jogo.
+2. Atualização dos inimigos (movimento, vida, posição, etc.) e da base (vida e créditos).
+3. Atualização das torres e dos seus efeitos sobre os inimigos.
+
+=== Exemplos de Uso:
+
+* `base` = Base {vidaBase = 100, posicaoBase = (2, 2), creditosBase = 50}
+* `portal` = Portal {posicaoPortal = (0, 0), ondasPortal = [Onda [] 5 0 0]}
+* `torre` = Torre (5, 5) 25 3 3 2 0 (Projetil Fogo (Finita 7))
+* `mapa` = [ [Terra, Relva, Agua, Relva, Terra], [Terra, Terra, Terra, Relva, Agua], [Agua, Relva, Terra, Relva, Relva] ]
+* `inimigo` = Inimigo (1, 1) Norte 100 1.0 10 20 [] (0, 0) 0
+* `jogo` = Jogo base [portal] [torre] mapa [inimigo] [] Um
+
+>>>
+atualizaJogo 0.3 jogo
+Jogo {
+baseJogo = Base {vidaBase = 100.0, posicaoBase = (2.0,2.0), creditosBase = 50}, 
+portaisJogo = [Portal {posicaoPortal = (0.0,0.0), ondasPortal = []}], 
+torresJogo = [Torre {posicaoTorre = (5.0,5.0), danoTorre = 25.0, alcanceTorre = 3.0, rajadaTorre = 3, cicloTorre = 2.0, tempoTorre = 0.0, projetilTorre = Projetil {tipoProjetil = Fogo, duracaoProjetil = Finita 7.0}}], 
+mapaJogo = [[Terra,Relva,Agua,Relva,Terra],[Terra,Terra,Terra,Relva,Agua],[Agua,Relva,Terra,Relva,Relva]], 
+inimigosJogo = [Inimigo {posicaoInimigo = (1.3,1.0), direcaoInimigo = Este, vidaInimigo = 100.0, velocidadeInimigo = 1.0, ataqueInimigo = 10.0, butimInimigo = 20, projeteisInimigo = [], posInicial = (0.0,0.0), tempoInimigo = 0.0}], 
+lojaJogo = [], 
+nivelJogo = Um }
+
+-}
+
 atualizaJogo :: Tempo -> Jogo -> Jogo
 atualizaJogo t (Jogo base portais torres mapa inimigos loja n) =
 
