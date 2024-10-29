7b8d8d677b7ec3906ca5cc2e4f3c84584dddf70a
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2024-10-29 12:28:31 +0000

    Writing examples for tests. v3
    Co-authored-by: veralmeida23 <2310veraalmeida@gmail.com>

--- spec/Tarefa3Spec.hs
+++ spec/Tarefa3Spec.hs
@@ -13,3 +13,7 @@ baseAtualiza = Base {vidaBase = 100, posicaoBase = (2, 2), creditosBase = 50}
 -- | Portal para teste da função atualizaJogo.
 portalAtualiza :: Portal
 portalAtualiza = Portal {posicaoPortal = (0, 0), ondasPortal = [Onda [] 5 0 0]}
+
+-- | Torre para teste da função atualizaJogo.
+torreAtualiza :: Torre
+torreAtualiza = Torre (5, 5) 25 3 3 2 0 (Projetil Fogo (Finita 7))
