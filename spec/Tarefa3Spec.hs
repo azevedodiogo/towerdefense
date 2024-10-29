ffa5930a4b6bf2c4f47e26e989033a5e036259a9
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2024-10-29 12:12:34 +0000

    Writing examples for tests.
    Co-authored-by: veralmeida23 <2310veraalmeida@gmail.com>

--- spec/Tarefa3Spec.hs
+++ spec/Tarefa3Spec.hs
@@ -3,3 +3,9 @@ module Tarefa3Spec where
 import Tarefa3
 import LI12425
 import Test.HUnit
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+-- | Base para teste da função atualizaJogo.
+baseAtualiza :: Base
+baseAtualiza = Base {vidaBase = 100, posicaoBase = (2, 2), creditosBase = 50}
