71585531210109d2d16b9e2c52070759ceda72cc
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2024-10-29 17:39:20 +0000

    Writing examples for tests. v12
    Co-authored-by: veralmeida23 <2310veraalmeida@gmail.com>

--- spec/Tarefa3Spec.hs
+++ spec/Tarefa3Spec.hs
@@ -54,3 +54,21 @@ listaInimigosFiltraID = [(0, Inimigo (3.0, 4.0) Norte 100.0 1.0 10.0 20 [] (3.0,
 -- | Lista de inimigos para teste da função filtraInimigoID.
 listaInimigosAtingidosFiltraID :: [(Int, Inimigo)]          
 listaInimigosAtingidosFiltraID = [(0, Inimigo (3.0, 4.0) Norte 100.0 1.0 10.0 20 [] (3.0, 4.0) 0), (1, Inimigo (7.0, 5.0) Sul 80.0 1.0 15.0 30 [] (7.0, 5.0) 0)]
+
+
+
+-- | Inimigo para teste da função atualizaOnda.
+inimigo1Atualiza :: Inimigo
+inimigo1Atualiza = Inimigo {posicaoInimigo = (1.3,1.0), direcaoInimigo = Este, vidaInimigo = 100.0, velocidadeInimigo = 1.0, ataqueInimigo = 10.0, butimInimigo = 20, projeteisInimigo = [], posInicial = (0.0,0.0), tempoInimigo = 0.0}
+
+-- | Inimigo para teste da função atualizaOnda.
+inimigo2Atualiza :: Inimigo
+inimigo2Atualiza = Inimigo {posicaoInimigo = (7.0,5.0), direcaoInimigo = Sul, vidaInimigo = 80.0, velocidadeInimigo = 1.0, ataqueInimigo = 15.0, butimInimigo = 30, projeteisInimigo = [], posInicial = (7.0,5.0), tempoInimigo = 0.0}
+
+-- | Onda para teste da função atualizaOnda.
+ondaAtualiza1 :: Onda
+ondaAtualiza1 = Onda {inimigosOnda = [inimigo1Atualiza, inimigo2Atualiza], cicloOnda = 10, tempoOnda = 5, entradaOnda = 0}
+
+-- | Onada para teste da função atualizaOnda.
+ondaAtualiza2 :: Onda
+ondaAtualiza2 = Onda {inimigosOnda = [], cicloOnda = 10, tempoOnda = 5, entradaOnda = 0}
