fdce82e431236edf6be81f85f5d16aa83660e408
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2024-10-30 17:30:07 +0000

    portal v2.
    Co-authored-by: veralmeida23 <2310veraalmeida@gmail.com>

--- app/Desenhar.hs
+++ app/Desenhar.hs
@@ -150,3 +150,10 @@ desenhaMapa mapa = translate (-910) 450 $
 portalesq :: Posicao -> Picture -> Picture
 portalesq (x, y) = translate (30 + x*60 - 910)      -- 60: medida de cada célula // -910: valor da translação do mapa // 30: valor, a meu ver, mais correto para ficar harmonioso.
                              (40 - y*60 + 450)      -- 60: medida de cada célula // 450: valor da translação do mapa // 40: valor, a meu ver, mais correto para ficar harmonioso.
+
+
+-- | Desenha o portal com a porta no lado direito (com terra do lado esquerdo)
+
+portaldir :: Posicao -> Picture -> Picture
+portaldir (x, y) = translate (-30 + x*60 - 910)     -- 60: medida de cada célula // -910: valor da translação do mapa // -30: valor, a meu ver, mais correto para ficar harmonioso
+                             (40 - y*60 + 450)      -- 60: medida de cada célula // 450: valor da translação do mapa // 40: valor, a meu ver, mais correto para ficar harmonioso
