85ebb5714167b52e6712c7b8dcc9bf7864cd23b0
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2024-11-01 15:32:02 +0000

    feat: wire verdeEscuro in Desenhar
    Co-authored-by: veralmeida23 <2310veraalmeida@gmail.com>

--- app/Desenhar.hs
+++ app/Desenhar.hs
@@ -40,3 +40,7 @@ verdeEscuro = makeColor (53/255) (121/255) (51/255) 1.0
 fundoRelva :: Int -> Int -> Picture -> Picture
 fundoRelva largura altura img = translate (-910) 450 $
                                 pictures [translate (fromIntegral x * 60) (fromIntegral (-y) * 60) $ scale 0.1 0.1 img | x <- [0..(largura-1)], y <- [0..(altura-1)]]
+
+
+
+-- | Desenha uma ponte de madeira horizontal
