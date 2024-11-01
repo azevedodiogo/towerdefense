67c817e96c9a03931054d4eaf116f45547e29364
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2024-11-01 11:31:17 +0000

    refactor: simplify corTerra in Desenhar
    Co-authored-by: veralmeida23 <2310veraalmeida@gmail.com>

--- app/Desenhar.hs
+++ app/Desenhar.hs
@@ -5,3 +5,8 @@ import ImmutableTowers
 import LI12425
 
 -- JogoRun
+
+
+-- MAPA
+
+-- só fica bem com mapas 21x16 (devido as medidas das celulas), caso o mapa seja menor, dps fica com bordas largas brancas dos lados
