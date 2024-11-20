b78b7eb44998c98ffc9f00b6d5bebbde655df8e9
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2024-11-20 10:03:41 +0000

    feat: draw iniOnda10 in ImmutableTowers
    Co-authored-by: veralmeida23 <2310veraalmeida@gmail.com>

--- app/ImmutableTowers.hs
+++ app/ImmutableTowers.hs
@@ -292,3 +292,7 @@ iniOnda06 =
     Inimigo (19,1) Oeste 50 1.0 12 10 [] (19,1) 0,
     Inimigo (19,1) Oeste 50 1.0 12 10 [] (19,1) 0,
     Inimigo (19,1) Oeste 50 1.0 12 10 [] (19,1) 0,
+    Inimigo (19,1) Oeste 50 1.0 12 10 [] (19,1) 0,
+    Inimigo (19,1) Oeste 50 1.0 12 10 [] (19,1) 0,
+    Inimigo (19,1) Oeste 50 1.0 12 10 [] (19,1) 0,
+    Inimigo (19,1) Oeste 50 1.0 12 10 [] (19,1) 0, 
