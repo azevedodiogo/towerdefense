88d17c369385b821267dafe446314f86a3bc4ea0
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2024-11-27 18:52:10 +0000

    refactor: simplify reageTempo in Tempo
    Co-authored-by: veralmeida23 <2310veraalmeida@gmail.com>

--- app/Tempo.hs
+++ app/Tempo.hs
@@ -5,3 +5,8 @@ import LI12425
 import Tarefa2
 import Tarefa3
 import Tarefa1
+
+
+-- | É chamada automaticamente em intervalos definidos no frame rate. Tem comportamentos específicos para os diferentes tipos do ImmutableTowers.
+
+reageTempo :: Tempo -> ImmutableTowers -> ImmutableTowers
