2dd568a261255e1e3a5daf0edd06db06a8fdd42a
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2024-11-27 18:55:57 +0000

    feat: add reageTempo in Tempo
    Co-authored-by: veralmeida23 <2310veraalmeida@gmail.com>

--- app/Tempo.hs
+++ app/Tempo.hs
@@ -10,3 +10,7 @@ import Tarefa1
 -- | É chamada automaticamente em intervalos definidos no frame rate. Tem comportamentos específicos para os diferentes tipos do ImmutableTowers.
 
 reageTempo :: Tempo -> ImmutableTowers -> ImmutableTowers
+
+reageTempo t (JogoRun jogo pos compra pz) | ganhouJogo jogo = Vitoria jogo pz
+                                          | perdeuJogo jogo = Derrota jogo pz
+                                          | otherwise = if validaJogo jogoAtualizado then JogoRun jogoAtualizado pos compra pz  -- verifica se o jogoAtualizado é valido, com a validaJogo da tarefa1
