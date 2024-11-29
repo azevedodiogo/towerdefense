d1ad08708ab552660fce1f7ee63e1f062a309db5
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2024-11-29 09:22:30 +0000

    refactor: clarify geraAleatorios in LI12425
    Co-authored-by: veralmeida23 <2310veraalmeida@gmail.com>

--- lib/LI12425.hs
+++ lib/LI12425.hs
@@ -2,3 +2,6 @@
 Module      : LI12425
 Description : Definições base do jogo
 Copyright   : Nelson Estevão <d12733@di.uminho.pt>
+              Olga Pacheco   <omp@di.uminho.pt>
+              Pedro Peixoto  <d14110@di.uminho.pt>
+              Xavier Pinho   <d12736@di.uminho.pt>
