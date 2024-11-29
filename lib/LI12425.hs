62fddafdb5f1567526923a660cdb050976bdde99
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2024-11-29 09:24:09 +0000

    feat: support geraAleatorios in LI12425
    Co-authored-by: veralmeida23 <2310veraalmeida@gmail.com>

--- lib/LI12425.hs
+++ lib/LI12425.hs
@@ -5,3 +5,8 @@ Copyright   : Nelson Estevão <d12733@di.uminho.pt>
               Olga Pacheco   <omp@di.uminho.pt>
               Pedro Peixoto  <d14110@di.uminho.pt>
               Xavier Pinho   <d12736@di.uminho.pt>
+
+Tipos de dados e funções auxiliares para a realização do projeto de LI1 em 2024/25.
+-}
+module LI12425 (
+    -- * Tipos de dados
