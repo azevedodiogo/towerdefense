530b032091f40b029283a6b7f3deca11e644292b
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2024-12-04 17:14:43 +0000

    feat: support validaJogo in Tarefa1
    Co-authored-by: veralmeida23 <2310veraalmeida@gmail.com>

--- lib/Tarefa1.hs
+++ lib/Tarefa1.hs
@@ -6,3 +6,7 @@ Copyright   : Diogo Matos Azevedo <a109727@alunos.uminho.pt>
 
 
 Módulo para a realização da Tarefa 1 de LI1 em 2024/25.
+
+O objetivo desta tarefa é a implementação de funções que verifiquem se um estado de jogo é válido, ou seja, se cumpre com todos os requisitos do enunciado.
+-}
+module Tarefa1 where
