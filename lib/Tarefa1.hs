6f1a85ab29392a0e0815e8d9af40a748bcb89585
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2024-12-04 17:25:46 +0000

    test: prepare validaJogo in Tarefa1
    Co-authored-by: veralmeida23 <2310veraalmeida@gmail.com>

--- lib/Tarefa1.hs
+++ lib/Tarefa1.hs
@@ -10,3 +10,8 @@ Módulo para a realização da Tarefa 1 de LI1 em 2024/25.
 O objetivo desta tarefa é a implementação de funções que verifiquem se um estado de jogo é válido, ou seja, se cumpre com todos os requisitos do enunciado.
 -}
 module Tarefa1 where
+
+import LI12425
+import Data.List
+
+{- | a função `validaJogo` verifica se um jogo é válido, ou seja, se cumpre com todos os requisitos, neste caso, se todas as funções desenvolvidas dão como resultado 'True'. -}
