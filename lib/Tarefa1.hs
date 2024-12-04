be544772e1665421d398835995c22e5e334d7e31
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2024-12-04 16:56:59 +0000

    refactor: clarify validaJogo in Tarefa1
    Co-authored-by: veralmeida23 <2310veraalmeida@gmail.com>

--- lib/Tarefa1.hs
+++ lib/Tarefa1.hs
@@ -2,3 +2,7 @@
 Module      : Tarefa1
 Description : Invariantes do Jogo
 Copyright   : Diogo Matos Azevedo <a109727@alunos.uminho.pt>
+              Vera da Silva Almeida <a110723@alunos.uminho.pt>
+
+
+Módulo para a realização da Tarefa 1 de LI1 em 2024/25.
