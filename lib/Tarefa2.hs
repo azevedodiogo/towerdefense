309665a0590293cd51cbbd91e91a81fc8437c964
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2024-12-13 10:23:21 +0000

    refactor: clarify inimigosNoAlcance in Tarefa2
    Co-authored-by: veralmeida23 <2310veraalmeida@gmail.com>

--- lib/Tarefa2.hs
+++ lib/Tarefa2.hs
@@ -2,3 +2,7 @@
 Module      : Tarefa2
 Description : Auxiliares do Jogo
 Copyright   : Diogo Matos Azevedo <a109727@alunos.uminho.pt>
+              Vera da Silva Almeida <a110723@alunos.uminho.pt>
+
+
+Módulo para a realização da Tarefa 2 de LI1 em 2024/25.
