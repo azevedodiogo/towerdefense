36177b1a4af519f1859838cef3806e3920b1e293
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2024-12-16 17:16:44 +0000

    refactor: clarify ativaInimigo in Tarefa2
    Co-authored-by: veralmeida23 <2310veraalmeida@gmail.com>

--- lib/Tarefa2.hs
+++ lib/Tarefa2.hs
@@ -152,3 +152,7 @@ tempo _ = 0
 
 
 === Exemplos de Uso:
+
+* `portal1` = Portal {posicaoPortal = (0.5, 0.5), ondasPortal = [onda1]}
+
+* `onda1` = Onda {inimigosOnda = [inimigo1, inimigo2], cicloOnda = 5, tempoOnda = 0, entradaOnda = 1}
