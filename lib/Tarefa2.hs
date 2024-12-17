d1c7935f9eb34124bec650208989934fe2bd2c10
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2024-12-17 13:27:51 +0000

    refactor: clarify perdeuJogo in Tarefa2
    Co-authored-by: veralmeida23 <2310veraalmeida@gmail.com>

--- lib/Tarefa2.hs
+++ lib/Tarefa2.hs
@@ -237,3 +237,8 @@ ondaVazia o = length (inimigosOnda o) == 0
 
 * `jogo1` = Jogo {baseJogo = base1, portaisJogo = [portal1], torresJogo = [], mapaJogo = mapa, inimigosJogo = [], lojaJogo = loja, nivelJogo = Um}
 * `jogo2` = Jogo {baseJogo = base2, portaisJogo = [], torresJogo = [], mapaJogo = mapa, inimigosJogo = [], lojaJogo = loja, nivelJogo = Um}
+
+* `base1` = Base {vidaBase = 100, posicaoBase = (5.5, 2.5), creditosBase = 50}
+* `base2` = Base {vidaBase = 0, posicaoBase = (5.5, 2.5), creditosBase = 50}
+* `portal1` = Portal {posicaoPortal = (0.5, 0.5), ondasPortal = [onda1]}
+* `onda1` = Onda {inimigosOnda = [inimigo1, inimigo2], cicloOnda = 5, tempoOnda = 0, entradaOnda = 1}
