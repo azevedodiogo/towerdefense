e1c4d84e41da50397f84357434f0a39b5c2a7f61
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2024-12-18 17:15:02 +0000

    feat: support baseJogo in Tarefa3
    Co-authored-by: veralmeida23 <2310veraalmeida@gmail.com>

--- lib/Tarefa3.hs
+++ lib/Tarefa3.hs
@@ -6,3 +6,6 @@ Copyright   : Diogo Matos Azevedo <a109727@alunos.uminho.pt>
 
 -- O propósito desta tarefa é simular um modelo de jogo, com um foco nos movimentos dos inimigos e na interação destes com as torres e a base.
 Módulo para a realização da Tarefa 3 de LI1 em 2024/25.
+-}
+
+module Tarefa3 where
