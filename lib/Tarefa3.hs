8f31295db7052668b02667df53418ea0631e14db
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2024-12-18 16:30:12 +0000

    refactor: clarify baseJogo in Tarefa3
    Co-authored-by: veralmeida23 <2310veraalmeida@gmail.com>

--- lib/Tarefa3.hs
+++ lib/Tarefa3.hs
@@ -2,3 +2,7 @@
 Module      : Tarefa3
 Description : Mecânica do Jogo
 Copyright   : Diogo Matos Azevedo <a109727@alunos.uminho.pt>
+              Vera da Silva Almeida <a110723@alunos.uminho.pt>
+
+-- O propósito desta tarefa é simular um modelo de jogo, com um foco nos movimentos dos inimigos e na interação destes com as torres e a base.
+Módulo para a realização da Tarefa 3 de LI1 em 2024/25.
