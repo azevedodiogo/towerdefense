0bf15f7f61988ce60e5101ff4e17b9550799adbf
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2024-12-20 09:43:39 +0000

    fix: validate atualizaJogo in Tarefa3
    Co-authored-by: veralmeida23 <2310veraalmeida@gmail.com>

--- lib/Tarefa3.hs
+++ lib/Tarefa3.hs
@@ -147,3 +147,8 @@ atualizaInimigoID t = map (\(id, ini) -> (id, atingeInimigo t ini))
 {- | a função `filtraInimigoID` retira, da lista inicial, todos os inimigos que foram atingidos pela torre. 
 
 === Exemplos de Uso:
+
+* `listaInimigos` = [(0, Inimigo (3.0, 4.0) Norte 100.0 1.0 10.0 20 [] (3.0, 4.0) 0),
+                     (1, Inimigo (7.0, 5.0) Sul 80.0 1.0 15.0 30 [] (7.0, 5.0) 0),
+                     (2, Inimigo (10.0, 10.0) Este 50.0 1.5 20.0 40 [] (10.0, 10.0) 0) ]
+          
