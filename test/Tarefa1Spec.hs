31d2416379e8f77b5b5af0afa233f33654955835
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2024-12-27 15:59:16 +0000

    fix: adjust inimigo01 in Tarefa1Spec
    Co-authored-by: veralmeida23 <2310veraalmeida@gmail.com>

--- test/Tarefa1Spec.hs
+++ test/Tarefa1Spec.hs
@@ -10,3 +10,8 @@ import Test.HUnit
 -- | Torre 1 para testes.
 torre01 :: Torre
 torre01 = Torre
+  { posicaoTorre = (3.0, 2.0),
+    danoTorre = 50.0,
+    alcanceTorre = 5.0,
+    rajadaTorre = 3,
+    cicloTorre = 2.0,
