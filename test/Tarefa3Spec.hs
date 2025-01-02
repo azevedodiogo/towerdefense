33c92127be3b2e4367ec0dd7347711ddfd71ff07
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-01-02 19:02:46 +0000

    test: cover mapaAtualiza in Tarefa3Spec
    Co-authored-by: veralmeida23 <2310veraalmeida@gmail.com>

--- test/Tarefa3Spec.hs
+++ test/Tarefa3Spec.hs
@@ -15,3 +15,7 @@ portalAtualiza :: Portal
 portalAtualiza = Portal {posicaoPortal = (0, 0), ondasPortal = [Onda [] 5 0 0]}
 
 -- | Torre para teste da função atualizaJogo.
+torreAtualiza :: Torre
+torreAtualiza = Torre (5, 5) 25 3 3 2 0 (Projetil Fogo (Finita 7))
+
+-- | Mapa para teste da função atualizaJogo.
