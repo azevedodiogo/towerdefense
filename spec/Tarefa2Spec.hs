43a5e8857d63012c1de5794edb48a041e60b304f
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-01-21 04:06:52 +0000

    Writing examples for testing. Projectile List 1.

--- spec/Tarefa2Spec.hs
+++ spec/Tarefa2Spec.hs
@@ -64,3 +64,7 @@ listaInimigos01 = [ Inimigo (3.0, 4.0) Norte 100.0 1.0 10.0 20 [] (3.0, 4.0) 0,
                     Inimigo (7.0, 5.0) Sul 80.0 1.0 15.0 30 [] (7.0, 5.0) 0,
                     Inimigo (10.0, 10.0) Este 50.0 1.5 20.0 40 [] (10.0, 10.0) 0
                     ]
+
+-- | Lista de projéteis para testes.
+listaProjeteis01 :: [Projetil]
+listaProjeteis01 = [Projetil Gelo (Finita 3), Projetil Gelo (Finita 5), Projetil Resina Infinita]
