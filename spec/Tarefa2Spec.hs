1dd1fa064e1d5e0fdd3f84ec993ebe06ccab3ab4
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-01-21 03:51:06 +0000

    Writing examples for tests. Enemy 3.

--- spec/Tarefa2Spec.hs
+++ spec/Tarefa2Spec.hs
@@ -13,3 +13,7 @@ inimigo01 = Inimigo (3.0, 4.0) Norte 100 1 10 20 [Projetil Fogo (Finita 5)] (0.5
 -- | Inimigo 2 para testes.
 inimigo02 :: Inimigo
 inimigo02 = Inimigo (7.0, 5.0) Sul 80 1 15 30 [Projetil Gelo (Finita 3)] (0.5,0.5) 0
+
+-- | Inimigo 3 para testes.
+inimigo03 :: Inimigo
+inimigo03 = Inimigo (10.0, 10.0) Este 50 1.5 20 40 [Projetil Resina Infinita] (0,0) 0
