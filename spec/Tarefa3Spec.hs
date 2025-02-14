815d5035d18e541080a96b70164d074e19d0ebb6
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-02-14 10:18:32 +0000

    Writing examples for tests. v2

--- spec/Tarefa3Spec.hs
+++ spec/Tarefa3Spec.hs
@@ -9,3 +9,7 @@ import Test.HUnit
 -- | Base para teste da função atualizaJogo.
 baseAtualiza :: Base
 baseAtualiza = Base {vidaBase = 100, posicaoBase = (2, 2), creditosBase = 50}
+
+-- | Portal para teste da função atualizaJogo.
+portalAtualiza :: Portal
+portalAtualiza = Portal {posicaoPortal = (0, 0), ondasPortal = [Onda [] 5 0 0]}
