04bd215ca06a25e886ef5276bf44ac4e89806d79
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-03-01 01:55:16 +0000

    Spec file completed.

--- spec/Spec.hs
+++ spec/Spec.hs
@@ -13,3 +13,6 @@ testSuite =
       [ "basic example test" ~: (2 :: Int) ~=? 1 + 1,
         "another basic example" ~: True ~=? not False
       ]
+
+main :: IO ()
+main = runTestTTAndExit $ test [testSuite, testesTarefa1, testesTarefa2, testesTarefa3]
