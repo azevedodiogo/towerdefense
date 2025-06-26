a63d2f4f8c4f0457c210d0f074387d0640f9eea4
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-06-26 01:04:55 +0100

    Horizontal wooden bridge.

--- app/Desenhar.hs
+++ app/Desenhar.hs
@@ -8,26 +8,38 @@ import LI12425
 
 -- CORES
 
--- | Cor da Terra
+-- | Cor da Terra.
 corTerra :: Color
 corTerra = makeColor (169 / 255) (122 / 255) (61 / 255) 1.0
 
--- | Cor da Água
+-- | Cor da Água.
 corAgua :: Color
 corAgua = makeColor (98/255) (189/255) (203/255) 1.0
 
 --(141/255) (222/255) (235/255) 1.0
 
--- | Cor para a sombra da terra
+-- | Cor para a sombra da terra.
 castanhoEscuro :: Color
 castanhoEscuro = makeColor (110 / 255) (80 / 255) (40 / 255) 1.0
 
--- | Cor para a sombra da relva
+-- | Cor para a sombra da relva.
 verdeEscuro :: Color
 verdeEscuro = makeColor (53/255) (121/255) (51/255) 1.0
 
--- | Desenha o fundo de relva
+-- | Desenha o fundo de relva.
 
 fundoRelva :: Int -> Int -> Picture -> Picture
 fundoRelva largura altura img = translate (-910) 450 $
                                 pictures [translate (fromIntegral x * 60) (fromIntegral (-y) * 60) $ scale 0.1 0.1 img | x <- [0..(largura-1)], y <- [0..(altura-1)]]
+
+-- | Desenha uma ponte de madeira horizontal.
+
+ponteDeMadeiraH :: Float -> Float -> Picture
+ponteDeMadeiraH x y = pictures (ponte ++ agua)
+
+    where ponte = [translate x y $ color castanhoEscuro $ rectangleSolid 66 66]
+
+          -- (3 retângulos com 15px cada e 7.5px de espaçamento)
+          agua = [ translate x (y + 18.75) $ color corAgua $ rectangleSolid 60 7.5,       -- Tábua 1
+                   translate x y $ color corAgua $ rectangleSolid 60 7.5,                 -- Tábua 2
+                   translate x (y - 18.75) $ color corAgua $ rectangleSolid 60 7.5  ]     -- Tábua 3
