edf091543644b6494d855c76b5bca4b9ab34051a
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-06-26 01:00:26 +0100

    Map colors i.e. water color, land color and grass color.

--- app/Desenhar.hs
+++ app/Desenhar.hs
@@ -3,3 +3,25 @@ module Desenhar where
 import Graphics.Gloss
 import ImmutableTowers
 import LI12425
+
+-- MAPA
+
+-- CORES
+
+-- | Cor da Terra
+corTerra :: Color
+corTerra = makeColor (169 / 255) (122 / 255) (61 / 255) 1.0
+
+-- | Cor da Água
+corAgua :: Color
+corAgua = makeColor (98/255) (189/255) (203/255) 1.0
+
+--(141/255) (222/255) (235/255) 1.0
+
+-- | Cor para a sombra da terra
+castanhoEscuro :: Color
+castanhoEscuro = makeColor (110 / 255) (80 / 255) (40 / 255) 1.0
+
+-- | Cor para a sombra da relva
+verdeEscuro :: Color
+verdeEscuro = makeColor (53/255) (121/255) (51/255) 1.0
