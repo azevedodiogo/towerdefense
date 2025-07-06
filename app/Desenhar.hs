384afe3e76b0ec2815a0ec3d6e3c7a1c65983c0b
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-07-06 13:19:00 +0000

    refactor: simplify portaldir in Desenhar

--- app/Desenhar.hs
+++ app/Desenhar.hs
@@ -176,3 +176,7 @@ portalesq (x, y) = translate (30 + x*60 - 910)      -- 60: medida de cada célul
 
 portaldir :: Posicao -> Picture -> Picture
 portaldir (x, y) = translate (-30 + x*60 - 910)     -- 60: medida de cada célula // -910: valor da translação do mapa // -30: valor, a meu ver, mais correto para ficar harmonioso
+                             (40 - y*60 + 450)      -- 60: medida de cada célula // 450: valor da translação do mapa // 40: valor, a meu ver, mais correto para ficar harmonioso
+
+
+-- | Desenha a base (com terra do lado direito)
