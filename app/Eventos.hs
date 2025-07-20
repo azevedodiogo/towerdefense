5ba3bc7fb29cbbe1cb937c9d8805833827fc66b0
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-07-20 11:28:00 +0000

    fix: handle reageEventos in Eventos

--- app/Eventos.hs
+++ app/Eventos.hs
@@ -2,3 +2,7 @@ module Eventos where
 
 import Graphics.Gloss.Interface.Pure.Game
 import ImmutableTowers
+import LI12425
+
+
+-- | Altera o estado do jogo conforme a tecla / botão do rato pressionado
