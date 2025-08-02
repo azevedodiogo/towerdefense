fa311cd15b19a249d088f19b2140b203e0a82aab
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-08-02 09:00:00 +0000

    refactor: simplify iniOnda04 in ImmutableTowers

--- app/ImmutableTowers.hs
+++ app/ImmutableTowers.hs
@@ -70,3 +70,7 @@ mapa01 =
       [r, r, r, t, r, r, r, t, r, r, r, r, r, r, r, r, r, t, r, r, r],
       [a, a, a, t, a, a, a, t, a, a, a, a, a, a, a, r, r, t, r, r, r],
       [r, r, r, t, r, r, r, t, r, r, r, r, r, r, a, r, r, t, r, r, r],
+      [r, r, r, t, r, r, r, t, r, r, r, r, t, t, t, t, t, t, r, r, r],
+      [r, r, r, t, r, r, r, t, r, r, r, r, t, r, a, r, r, r, r, r, r],
+      [r, r, r, t, r, r, r, t, r, r, r, r, t, r, a, r, r, r, r, r, r],
+      [r, r, r, t, r, r, r, t, r, r, r, r, t, r, a, r, r, r, r, r, r],
