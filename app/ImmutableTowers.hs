8f6db090ba1c6e1a2ec3192016427213d79492ff
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-08-15 10:14:00 +0000

    fix: handle iniOnda14 in ImmutableTowers

--- app/ImmutableTowers.hs
+++ app/ImmutableTowers.hs
@@ -493,3 +493,7 @@ iniOnda11 =
     Inimigo (1,3) Este 20 0.8 10 5 [] (1,3) 0,
     Inimigo (1,3) Este 20 0.8 10 5 [] (1,3) 0, 
     Inimigo (1,3) Este 20 0.8 10 5 [] (1,3) 0,
+    Inimigo (1,3) Este 20 0.8 10 5 [] (1,3) 0,
+    Inimigo (1,3) Este 20 0.8 10 5 [] (1,3) 0,
+    Inimigo (1,3) Este 20 0.8 10 5 [] (1,3) 0,
+    Inimigo (1,3) Este 20 0.8 10 5 [] (1,3) 0,
