ff649d180117d78f2ccf04dc351f1836aa03a02c
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-09-19 13:19:00 +0000

    test: prepare verificaRajadaTorre in Tarefa1

--- lib/Tarefa1.hs
+++ lib/Tarefa1.hs
@@ -523,3 +523,7 @@ verificaCicloTorre j = all verificaCicloTorre2 (torresJogo j)
 === Exemplo de Uso:
 
 * `jogo1` = Jogo {baseJogo = base, portaisJogo = [], torresJogo = [], mapaJogo = mapa, inimigosJogo = [Inimigo (2,0) Sul 80.0 1.0 15.0 30 [] (0, 0) 0], lojaJogo = [], nivelJogo = Um}
+* `jogo2` = Jogo {baseJogo = base, portaisJogo = [], torresJogo = [Torre (2, 0) 25 3 3 2 0 (Projetil Fogo (Finita 7)), Torre (2, 0) 25 (-3) 3 2 0 (Projetil Fogo (Finita 7))], mapaJogo = mapa, inimigosJogo = [Inimigo (1, 1) Sul 80.0 1.0 15.0 30 [] (0, 0) 0], lojaJogo = [], nivelJogo = Um}
+
+* `mapa` = [ [t, t, r, a, a, a], [r, t, r, a, r, r], [r, t, r, a, r, t], [r, t, r, a, r, t], [r, t, t, t, t, t], [a, a, a, a, r, r] ]
+ 
