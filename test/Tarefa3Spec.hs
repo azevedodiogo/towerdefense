27bf39396d9ac3d126fad5f364330b0170bc6671
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-11-04 09:37:00 +0000

    test: exercise portalAtualiza in Tarefa3Spec

--- test/Tarefa3Spec.hs
+++ test/Tarefa3Spec.hs
@@ -5,3 +5,7 @@ import LI12425
 import Test.HUnit
 
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+-- | Base para teste da função atualizaJogo.
+baseAtualiza :: Base
+baseAtualiza = Base {vidaBase = 100, posicaoBase = (2, 2), creditosBase = 50}
