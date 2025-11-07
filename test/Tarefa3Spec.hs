648b60f00973868d03dc1f7b798800f0e5e8c930
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-11-07 13:19:00 +0000

    test: cover inimigosHitBase in Tarefa3Spec

--- test/Tarefa3Spec.hs
+++ test/Tarefa3Spec.hs
@@ -144,3 +144,7 @@ baseIniSemVida :: Base
 baseIniSemVida = Base {vidaBase = 100, posicaoBase = (2.2, 2.2), creditosBase = 50}
 
 -- | Lista de inimigos para teste da função removeInimigosSemVida.
+inimigosIniSemVida :: [Inimigo]
+inimigosIniSemVida = [Inimigo (2, 2) Este 0 1.0 5 25 [Projetil Resina Infinita] (0.5, 0.5) 0, Inimigo (0.5, 1.0) Sul 100 1.0 5 25 [Projetil Gelo (Finita 2)] (0.5, 0.5) 0]
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
