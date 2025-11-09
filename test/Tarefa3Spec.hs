5f100a58fc09cd150dabaf90d7000cd1bf0b8e2e
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-11-09 12:42:00 +0000

    test: exercise testeAtualizaOnda in Tarefa3Spec

--- test/Tarefa3Spec.hs
+++ test/Tarefa3Spec.hs
@@ -178,3 +178,7 @@ testeCaminho :: Test
 testeCaminho = "Teste 7" ~: [(0,0),(0,1),(1,1),(2,1),(2,2)] ~=? caminho (0, 0) (2, 2) mapaCaminho
 
 -- | Teste da função direcaoToOeste.
+testaDirecaoToOeste :: Test
+testaDirecaoToOeste = "Teste 8" ~: Sul ~=? direcaoToOeste Norte (3, 2) [(3, 3), (4, 3)]
+
+-- | Teste da função atualizaPosicao.
